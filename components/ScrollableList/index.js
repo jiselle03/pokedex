49c45bd45eb3b8ac7e4da1b9be522b802e2cb472
Jiselle Liu <jiselle03@gmail.com>
2020-02-19 11:30:20 -0800

    Fix refactoring bugs and add TouchableHighlight

--- components/ScrollableList/index.js
+++ components/ScrollableList/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { StyleSheet, ScrollView, View, Text } from 'react-native';
+import { StyleSheet, ScrollView, View, Text, TouchableHighlight } from 'react-native';
 
 export default function ScrollableList({items}) {
     return(
         <ScrollView style={styles.pokemonList}>
-        {pokemonList.map(pokemon => {
+        {items.map(pokemon => {
           return (
-            <View key={pokemon.name} style={styles.item}>
-              <Text style={styles.h2}>{pokemon.name}</Text>
-            </View>
+            <TouchableHighlight onPress={() => console.log('tapped')}>
+                <View key={pokemon.name} style={styles.item}>
+                    <Text style={styles.h2}>{pokemon.name}</Text>
+                </View>
+            </TouchableHighlight>
           )
         })}
-      </ScrollView>
+        </ScrollView>
     );
 };
 
@@ -19,10 +21,9 @@ const styles = StyleSheet.create({
     pokemonList: {
         flex: 1,
         flexDirection: 'column',
-        alignItems: 'stretch',
       },
       item: {
-        backgroundColor: 'pink',
+        backgroundColor: 'white',
         alignItems: 'center',
         borderBottomWidth: 2,
         height: 40,
